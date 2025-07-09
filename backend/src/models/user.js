9b83fad093e0608bf597aac1dc8cbef6b9db0d8c
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2025-07-09 20:17:50 +0530

    API level data sanitization.

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,25 +2,52 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
-        type: String
+        type: String,
+        required: true,
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18
     },
     gender: {
-        type: String
-    }
+        type: String,
+        validate(value) {
+            if (!["male", "female", "other"].includes(value)) {
+                throw new Error ("Gender data is not valid.");
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://www.clipartmax.com/middle/m2i8b1b1i8A0Z5H7_shivaprakash-b-dummy-user/",
+    },
+    about: {
+        type: String,
+        default: "Default user"
+    },
+    skills: {
+        type: [String]
+    },
+}, {
+    timestamps: true
 });
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
